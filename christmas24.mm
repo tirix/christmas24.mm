060653f50ab45be37c41b8362b754998880ad613
Thierry Arnoux <5831830+tirix@users.noreply.github.com>
2024-12-02 08:44:37 +0100

    First day of Christmas, `abaeqb`

---
+++ christmas24.mm
@@ -0,0 +1,112 @@
+$[ set.mm $]
+
+$( ADVENT OF METAMATH 2024!
+
+   The task is to prove several results about magmas, some of them appearing in the Putnam exam,
+   one being the famous Eckmann-Hilton argument, see https://en.wikipedia.org/wiki/Eckmann%E2%80%93Hilton_argument ,
+   while the last one is the result of Mendelsohn and Padmanabhan, that a certain identity characterizes boolean groups.
+
+   Since most of those results concern universal algebra, not all of them are formulated in terms of set.mm's magma structure ` Mgm ` ,
+   but it should be fairly easy to convert them to that form if needed.
+   Have fun!
+$)
+
+  ${
+    $d .o. a b $. $d B a b $. $d X a b $. $d Y a b $. $d a ph $.
+    abaeqb.a $e |- ( ph -> A. a e. B A. b e. B ( a .o. b ) e. B ) $.
+    abaeqb.b $e |- ( ph -> A. a e. B A. b e. B ( a .o. ( b .o. a ) ) = b ) $.
+    abaeqb.x $e |- ( ph -> X e. B ) $.
+    abaeqb.y $e |- ( ph -> Y e. B ) $.
+    $( If for all ` a, b ` in ` B ` we have ` a .o. ( b .o. a ) = b ` , then we
+        also have ` ( a .o. b ) .o. a = b ` . $)
+    abaeqb $p |- ( ph -> ( ( X .o. Y ) .o. X ) = Y ) $=
+      ( co wceq wral id oveq2 oveq1 oveq2d wa eqidd oveq12d eqeq12d rspc2vd mpd
+      cv eqeq1d wcel eleq1d eqtr3d ) ACDELZDUJELZELZUJCELDAUKCUJEAFUEZGUEZUMELZ
+      ELZUNMZGBNFBNZUKCMZIAUSDUNDELZELZUNMUQFGDCBBBUMDMZUPVAUNVBUMDUOUTEVBOUMDU
+      NEPUAUFUNCMZVAUKUNCVCUTUJDEUNCDEQRVCOUBKAVBSBTJUCUDRAURULDMZIAVDUJUNUJELZ
+      ELZUNMUQFGUJDBBBUMUJMZUPVFUNVGUMUJUOVEEVGOUMUJUNEPUAUFUNDMZVFULUNDVHVEUKU
+      JEUNDUJEQRVHOUBAUMUNELZBUGZGBNFBNUJBUGZHAVKCUNELZBUGVJFGCDBBBUMCMZVIVLBUM
+      CUNEQUHVHVLUJBUNDCEPUHJAVMSBTKUCUDAVGSBTKUCUDUI $.
+  $}
+
+${
+  $( If every element is an idempotent and a certain "cyclic" identity holds, then the operation is commutative. $)
+  $d a B X Y .o. $. $d b B X Y .o. $. $d c B X Y .o. $.
+  idemp.a $e |- ( ph -> A. a e. B A. b e. B ( a .o. b ) e. B ) $.
+  idemp.b $e |- ( ph -> A. a e. B ( a .o. a ) = a ) $.
+  idemp.c $e |- ( ph -> A. a e. B A. b e. B A. c e. B ( ( a .o. b ) .o. c ) = ( ( b .o. c ) .o. a ) ) $.
+  idemp.d $e |- ( ph -> X e. B ) $.
+  idemp.e $e |- ( ph -> Y e. B ) $.
+  idemp $p |- ( ph -> ( X .o. Y ) = ( Y .o. X ) ) $= ? $.
+$}
+
+${
+  $( If multiplying two times from the left (and the right) is an identity mapping, then the operation is commutative. $)
+  $d a B X Y .o. $. $d b B X Y .o. $. $d c B X Y .o. $.
+  aabbaa.a $e |- ( ph -> A. a e. B A. b e. B ( a .o. b ) e. B ) $.
+  aabbaa.b $e |- ( ph -> A. a e. B A. b e. B ( a .o. ( a .o. b ) ) = b ) $.
+  aabbaa.c $e |- ( ph -> A. a e. B A. b e. B ( ( b .o. a ) .o. a ) = b ) $.
+  aabbaa.d $e |- ( ph -> X e. B ) $.
+  aabbaa.e $e |- ( ph -> Y e. B ) $.
+  aabbaa $p |- ( ph -> ( X .o. Y ) = ( Y .o. X ) ) $= ? $.
+$}
+
+${
+  $( The Eckmann-Hilton argument. If for two unital magmas, represented here by the operation ` .o. ` with unit ` E `
+     and the operation ` .(x) ` with unit ` I ` , on the same base set ` B ` an identity ` ( ( a .(x) b ) .o. ( c .(x) d ) ) = ( ( a .o. c ) .(x) ( b .o. d ) ) `
+     holds, then these magmas coincide and the operation is associative and commutative. $)
+  $d a B E I .o. .(x) $. $d b B E I .o. .(x) $. $d c B E I .o. .(x) $.
+  eckmannhilton.a $e |- ( ph -> A. a e. B A. b e. B ( a .o. b ) e. B ) $.
+  eckmannhilton.b $e |- ( ph -> A. a e. B A. b e. B ( a .(x) b ) e. B ) $.
+  eckmannhilton.c $e |- ( ph -> A. a e. B ( ( a .o. E ) = a /\ ( E .o. a ) = a ) ) $.
+  eckmannhilton.d $e |- ( ph -> A. a e. B ( ( a .(x) I ) = a /\ ( I .(x) a ) = a ) ) $.
+  eckmannhilton.e $e |- ( ph -> ( E e. B /\ I e. B ) ) $.
+  eckmannhilton.f $e |- ( ph -> A. a e. B A. b e. B A. c e. B A. d e. B ( ( a .(x) b ) .o. ( c .(x) d ) ) = ( ( a .o. c ) .(x) ( b .o. d ) ) ) $.
+  eckmannhilton $p |- ( ph -> ( E = I /\ A. a e. B A. b e. B ( ( a .o. b ) = ( a .(x) b ) /\ ( a .o. b ) = ( b .o. a ) ) /\
+                        A. a e. B A. b e. B A. c e. B ( ( a .o. b ) .o. c ) = ( a .o. ( b .o. c ) ) ) ) $= ? $.
+$}
+
+${
+  $( Lemma for mendpadm. Multiplication is surjective. $)
+  $d x B M .o. $. $d y B M .o. $. $d z B M .o. $.
+  mendpadm.a $e |- B = ( Base ` M ) $.
+  mendpadm.b $e |- .o. = ( +g ` M ) $.
+  mendpadm.c $e |- ( ph -> B =/= (/) ) $.
+  mendpadm.d $e |- ( ph -> M e. Mgm ) $.
+  mendpadm.e $e |- ( ph -> A. x e. B A. y e. B A. z e. B ( ( x .o. y ) .o. ( x .o. ( z .o. y ) ) ) = z ) $.
+  mendpadmlem1 $p |- ( ph -> A. z e. B E. x e. B E. y e. B  ( x .o. y ) = z ) $= ? $.
+
+  $( Lemma for mendpadm. Multiplication by ` x ` from the left is surjective. $)
+  mendpadmlem2 $p |- ( ph -> A. z e. B A. x e. B E. y e. B  ( x .o. y ) = z ) $= ? $.
+
+  $( Lemma for mendpadm. Multiplication by ` y ` from the right is surjective. $)
+  mendpadmlem3 $p |- ( ph -> A. z e. B A. y e. B E. x e. B  ( x .o. y ) = z ) $= ? $.
+
+  $( Lemma for mendpadm. The identity ` x .o. ( y .o. x ) = y ` holds. $)
+  mendpadmlem4 $p |- ( ph -> A. x e. B A. y e. B ( x .o. ( y .o. x ) ) = y ) $= ? $.
+
+  $( Lemma for mendpadm. Multiplication of ` x ` by ` x .o. x ` gives ` x ` . $)
+  mendpadmlem5 $p |- ( ph -> A. x e. B ( ( x .o. ( x .o. x ) ) = x /\ ( ( x .o. x ) .o. x ) = x ) ) $= ? $.
+
+  $( Lemma for mendpadm. Squares of all elements are equal to each other. $)
+  mendpadmlem6 $p |- ( ph -> A. x e. B A. y e. B ( x .o. x ) = ( y .o. y ) ) $= ? $.
+
+  $( Lemma for mendpadm. ` M ` has an identity element. $)
+  mendpadmlem7 $p |- ( ph -> ( 0g ` M ) e. B ) $= ? $.
+
+  $( Lemma for mendpadm. Multiplying two times from the left or right by the same element gives the original element. $)
+  mendpadmlem8 $p |- ( ph -> A. x e. B A. y e. B ( ( x .o. ( x .o. y ) ) = y /\ ( ( x .o. y ) .o. y ) = x ) ) $= ? $.
+
+  $( Lemma for mendpadm. Multiplication is commutative. $)
+  mendpadmlem9 $p |- ( ph -> A. x e. B A. y e. B ( x .o. y ) = ( y .o. x ) ) $= ? $.
+
+  $( Lemma for mendpadm. Multiplication is associative. $)
+  mendpadmlem10 $p |- ( ph -> A. x e. B A. y e. B A. z e. B ( ( x .o. y ) .o. z ) = ( x .o. ( y .o. z ) ) ) $= ? $.
+
+  $( Lemma for mendpadm. ` M ` is a group. $)
+  mendpadmlem11 $p |- ( ph -> M e. Grp ) $= ? $.
+
+  $( Boolean groups (i.e. abelian groups of exponent 2), can be defined by one identity.
+     Here we use a short identity from a 1972 paper of N.S. Mendelsohn and R. Padmanabhan. $)
+  mendpadm $p |- ( ph -> ( M e. Abel /\ A. x e. B ( x .o. x ) = ( 0g ` M ) ) ) $= ? $.
+$}
